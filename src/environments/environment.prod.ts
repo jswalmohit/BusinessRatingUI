f96d9938a953cf1b866c725ca9ce325432a7998f
Mohit <mohitjaiswal.bltr@gmail.com>
2025-03-14 00:45:13 +0530

    initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    API_KEY: 'AlzaSyAEkLmrf0uGoPkSddyhngXvEJp52-Fdwdj',
+    baseUrl:'https://business-11.onrender.com/api/'
+
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    API_KEY: 'AlzaSyAEkLmrf0uGoPkSddyhngXvEJp52-Fdwdj',
+    baseUrl:'https://localhost:7000/api/'
+  };
